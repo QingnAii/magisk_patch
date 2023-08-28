0577227aea7cd453d02d1a1bf5af5d00e911a2a0
QingnAii <87488562+QingnAii@users.noreply.github.com>
2023-08-29 00:04:20 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
  ls *.img | while read i; do
         [[ "$i" = "$MODID" ]] && continue
         echo 正在修补$i
-        echo 26.1
+        echo 26.2
         sh boot_patch.sh $i
         echo "$i is done"
         echo $i 完成 >>log.txt
